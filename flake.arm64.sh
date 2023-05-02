011ec2c1b8f2d7256ec937ce5f50ac39493b1029
Titov Anton <webdev@titovanton.com>
2023-05-02 21:22:43 +0700

    macbook flake runner fix

--- flake.arm64.sh
+++ flake.arm64.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-alias flake='id=$(docker ps --filter name=flake --format "{{.ID}}") && \
-             if [ ! -z "$id" ]; then docker kill "$id"; fi && \
-             docker run --rm -ti -v $PWD:/watch_dir:ro --name=flake titovanton/flake:arm64'
+
+kill_and_run_flake() {
+    id=$(docker ps --filter name=flake --format "{{.ID}}")
+    if [ ! -z "$id" ]; then docker kill "$id"; fi
+    docker run --rm -ti -v $PWD:/watch_dir:ro --name=flake titovanton/flake:arm64
+}
+
+
+alias flake="kill_and_run_flake"
