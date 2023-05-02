72fedada1e9eff1cfb4d80c421b8df9ec3e4b790
Titov Anton <webdev@titovanton.com>
2023-05-02 19:08:28 +0500

    new python setup for arm64

--- flake.arm64.sh
+++ flake.arm64.sh
@@ -2,4 +2,4 @@
 
 alias flake='id=$(docker ps --filter name=flake --format "{{.ID}}") && \
              if [ ! -z "$id" ]; then docker kill "$id"; fi && \
-             docker run --rm -ti -v $PWD:/app:ro --name=flake titovanton/flake:arm64'
+             docker run --rm -ti -v $PWD:/watch_dir:ro --name=flake titovanton/flake:arm64'
