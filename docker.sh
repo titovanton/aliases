744e4c697886617c7d6d928dcc9f22be974d121c
Titov Anton <webdev@titovanton.com>
2023-12-27 05:19:19 +0500

    sorting aliases

--- docker.sh
+++ docker.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+alias dc=dc_docker_env_func
+alias dcb='dc build'
+alias dcd='dc down --remove-orphans'
+alias dcdbue=down_build_up_exec
+alias dcdbul=down_build_up_log
+alias dcdr='dcd && dcr'
+alias dcdrr='dcd && dcrr'
+alias dcdu='dcd && dcu'
+alias dcdue='dcd && dcu && dce'
+alias dcdul='dcd && dcu && dcl'
+alias dce=compose_exec_func
+alias dcfu=compose_up_f_d_func
+alias dcl='dc logs -f'
+alias dcr=compose_run_func
+alias dcrr='dc run --rm --service-ports'
+alias dcs='dc ps --services'
+alias dcsu=restart_func
+alias dcu='dc up -d'
+alias dcue='dcu && dce'
+alias dcul='dcu && dcl'
+alias de=docker_exec_func
+alias dk='docker kill'
+alias dnr='docker rmi -f $(docker images -f dangling=true -q)'
+alias dps='docker ps --format "{{.ID}}\t{{.Names}}"'
+
 dc_docker_env_func() {
   if [ $DOCKER_ENV ]; then
     docker compose -f docker-compose-$DOCKER_ENV.yml  $@
@@ -7,18 +32,6 @@ dc_docker_env_func() {
     docker compose  $@
   fi
 }
-
-alias dc=dc_docker_env_func
-
-alias dps='docker ps --format "{{.ID}}\t{{.Names}}"'
-alias dcs='dc ps --services'
-alias dcu='dc up -d'
-alias dcd='dc down --remove-orphans'
-alias dcb='dc build'
-alias dcl='dc logs -f'
-alias dk='docker kill'
-
-alias dnr='docker rmi -f $(docker images -f dangling=true -q)'
 
 restart_func() {
   dc stop $1 && dc up -d $1
@@ -72,18 +85,3 @@ down_build_up_log() {
   fi
 }
 
-alias restart=restart_func
-alias dce=compose_exec_func
-alias dcr=compose_run_func
-alias dcrr='dc run --rm --service-ports'
-alias de=docker_exec_func
-alias dcfu=compose_up_f_d_func
-alias dcdu='dcd && dcu'
-alias dcue='dcu && dce'
-alias dcdue='dcd && dcu && dce'
-alias dcul='dcu && dcl'
-alias dcdul='dcd && dcu && dcl'
-alias dcdr='dcd && dcr'
-alias dcdrr='dcd && dcrr'
-alias dcdbue=down_build_up_exec
-alias dcdbul=down_build_up_log
