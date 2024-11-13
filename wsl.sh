8cc6c27dc6fa2df794f2908d7d88164efab63f97
Titov Anton <anton.titov.webdev@gmail.com>
2024-11-13 22:16:50 +0500

    WSL better logging

--- wsl.sh
+++ wsl.sh
@@ -1,22 +1,27 @@
-function __wslu {
-    # mount disk to the Ubuntu fs
-    wsl -d Ubuntu --mount D:/VHDX/apps.vhdx --vhd --bare > /dev/null
+__apps_disk_label='Z:'
 
-    # keeps wsl alive
+function __wslu {
+    echo -e "\nMount vhdx disk to Ubuntu FS"
+    wsl -d Ubuntu --mount D:/VHDX/apps.vhdx --vhd --bare
+
+    echo -e "\nRun WSL and keep it alive"
     wsl --exec dbus-launch true
 
-    # mount apps to the Win fs
+    echo -e "\nMount 'apps' to Win fs as '$__apps_disk_label' disk"
     ip=$(
         wsl -d Ubuntu ip addr show eth0 | \
         grep 'inet ' | awk '{print $2}' | \
         cut -d/ -f1 | \
         xargs
     )
-    net use Z: \\\\$ip\\apps
+    net use $__apps_disk_label \\\\$ip\\apps
 }
 
 function __wsld {
+    echo -e "\nDelete '$__apps_disk_label'"
     net use Z: //delete
+
+    echo -e "\nShut down WSL"
     wsl -d Ubuntu --shutdown
 }
 
